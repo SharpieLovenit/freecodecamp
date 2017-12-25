a5662f77a80d77d526961afb92064efde138718e
Stephan Olivier <sharpie.lovenit@gmail.com>
2017-12-25 21:48:19 +0100

    Changes API call to GetJSON and added code for processing channel data

--- twitch-tv/js/twitch.js
+++ twitch-tv/js/twitch.js
@@ -1,24 +1,57 @@
-$(document).ready(function() {
+var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var endpoint = 'https://api.twitch.tv/kraken/';
+var client_id = '3917l4p9kj2yrbkv3a0g53hdxofddv';
 
-    $('.btn-group > .btn').click(function(){
+$(document).ready(function () {
+    var channelData;
+    $('.btn-group > .btn').click(function () {
         $(".btn-group > .btn").removeClass("active");
         $(this).addClass("active");
-        //alert("Button clicked, id "+this.id+", text "+this.innerHTML);
-        getTwitchChannels();
+        //console.log("Button clicked, id "+this.id+", text "+this.innerHTML);
+        getTwitchChannels(this.id);
     });
 
-function getTwitchChannels(){
-    $.ajax({
-        type: 'GET',
-        url: 'https://api.twitch.tv/kraken/channels/twitch',
-        headers: {
-        'Client-ID': '3917l4p9kj2yrbkv3a0g53hdxofddv'
-        },
-         success: function(data) {
-           console.log(data);
-         }
-    });
-   
-}
+    function buildURL(type, channel) {
+        return endpoint + type + '/' + channel + '?client_id=' + client_id;
+    }
 
+    function listChannel(isOnline, channel, channelData, streamData) {
+        var status = isOnline ? 'online' : 'offline';
+
+        name = channelData.display_name;
+        logo = channelData.logo;
+        url = channelData.url;
+
+        if (isOnline) {
+            game = streamData.game;
+        }
+        else {
+            game = 'Offline'
+        }
+
+        console.log('name: ' + name);
+        console.log('logo: ' + logo);
+        console.log('url: ' + url);
+        console.log('game: ' + game);
+
+    }
+
+    function getTwitchChannels(filter) {
+        channels.forEach(function (channel) {
+            $.getJSON(buildURL('streams', channel), function (data) {
+                var isOnline = data.stream ? true : false;
+
+                if (isOnline && (filter == "btn-all" || filter == "btn-online")) {
+                    listChannel(true, channel, data.stream.channel, data.stream);
+                }
+                else if (!isOnline && (filter == "btn-all" || filter == "btn-offline")) {
+                    // Get the channel info with a separate API call as this is not
+                    // returned for offline streams
+                    $.getJSON(buildURL('channels', channel), function (data2) {
+                        listChannel(false, channel, data2);
+                    });
+                }
+            });
+        });
+    }
 });
